a0f1622d0918de42e51f87869caa984c96c00cbe
Dylan <DylanAPetrae@gmail.com>
2017-11-17 18:13:34 -0500

    Added Employee Main Activity, Button Styling, and onClicks to login and logout buttons.

--- app/src/main/java/com/example/android/employeetracker/AdminMainActivity.java
+++ app/src/main/java/com/example/android/employeetracker/AdminMainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.employeetracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminMainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AdminMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+    }
+
+    public void logoutUser( View view ){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/android/employeetracker/MainActivity.java
+++ app/src/main/java/com/example/android/employeetracker/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.employeetracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void loginUser( View view ){
+
+        // We should make a conditional block here to determine whether the inputted
+        // login information is for an employee or admin account
+
+        // If it is an admin's account, start the Admin Main Activity (control panel)
+        Intent i = new Intent(this, AdminMainActivity.class);
+        startActivity(i);
+
+        // If it is an employee's account, start the Employee Main Activity
+        // Intent i = new Intent( packageContext: this, AdminMainActivity.class);
+        // startActivity(i);
+    }
+
 }
