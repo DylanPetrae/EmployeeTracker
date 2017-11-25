c2a96e070c43db46aaa27dea031879ecd8502e2b
Charlene May <charlenemayy@gmail.com>
2017-11-25 17:35:16 -0500

    updating files

--- app/src/main/java/com/example/android/employeetracker/EmployeeMainActivity.java
+++ app/src/main/java/com/example/android/employeetracker/EmployeeMainActivity.java
@@ -1,51 +1,54 @@
 package com.example.android.employeetracker;
 
-import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Date;
 
+
 public class EmployeeMainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    // Firebase methods
     private FirebaseAuth firebaseAuth;
-
-    // Location methods
-    LocationManager locationManager;
-    LocationListener locationListener;
-
-    private FirebaseUser user;
 
     private TextView textViewUserEmail;
 
     private Button buttonLogout;
+
+    private Bitmap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_main);
 
+
         firebaseAuth = FirebaseAuth.getInstance();
 
-
         FirebaseUser user = firebaseAuth.getCurrentUser();
-
 
         // If user is NOT logged in
         if(firebaseAuth.getCurrentUser() == null) {
@@ -54,7 +57,36 @@ public class EmployeeMainActivity extends AppCompatActivity implements View.OnCl
 
         }
 
-        user = firebaseAuth.getCurrentUser();
+        // Take user to camera immediately after log in
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(cameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, 1); // Not calling OnActivityResult; therefore map is null
+        }
+
+        // Store image to bitmap
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        map.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        // Create reference to filepath
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference userImageRef = storageRef.child("images/" + user.getUid() + ".jpg");
+
+        // Upload to Firebase Storage
+        UploadTask uploadTask = userImageRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+            }
+        });
 
         //Intialize Instace of FireStore
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -74,6 +106,7 @@ public class EmployeeMainActivity extends AppCompatActivity implements View.OnCl
         //update to 'users' data
         db.collection("users").document(user.getUid()).update(userMap);
 
+
         textViewUserEmail = (TextView) findViewById(R.id.welcomeUser);
 
         textViewUserEmail.setText("Welcome "+user.getEmail());
@@ -81,35 +114,25 @@ public class EmployeeMainActivity extends AppCompatActivity implements View.OnCl
         buttonLogout = (Button) findViewById(R.id.employeeLogoutButton);
 
         buttonLogout.setOnClickListener(this);
-
-        // Location
-        // TODO: Check if GPS is enables. Else, request permission
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new MyLocationListener(user);
-
-        try {
-            // Store GPS every 5 minutes (300000 milliseconds)
-            locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER,5000, 0, locationListener);
-        }
-        catch (SecurityException e) {
-            //ERROR
-            System.err.println("ERROR: Couldn't retrieve GPS.");
-        }
-
-    }
-
-    public FirebaseUser getUser(){
-        return user;
     }
 
     @Override
     public void onClick(View view){
         if(view == buttonLogout){
             firebaseAuth.signOut();
-            locationManager.removeUpdates(locationListener);
-            locationManager = null;
             finish();
             startActivity(new Intent(this, MainActivity.class));
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Store image to database
+        Log.d("myTag", "accessed" );
+        if(requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap map = (Bitmap) extras.get("data");
+        }
+
     }
 }
