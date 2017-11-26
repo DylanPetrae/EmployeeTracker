7bed9ff27ee7a3a1f6f0a5da4486f316dd922ead
Dylan <DylanAPetrae@gmail.com>
2017-11-26 18:59:58 -0500

    Added Employee List populating

--- app/src/main/java/com/example/android/employeetracker/EmployeeListActivity.java
+++ app/src/main/java/com/example/android/employeetracker/EmployeeListActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.employeetracker;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,8 +15,15 @@ import com.bumptech.glide.Glide;
 import com.example.android.employeetracker.AdminMainActivity;
 import com.example.android.employeetracker.R;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.Map;
 
 
 public class EmployeeListActivity extends AppCompatActivity {
@@ -30,6 +38,27 @@ public class EmployeeListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_employee);
 
         textViewEmployeeList = (TextView) findViewById(R.id.textViewEmployeeList);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("users").document("list");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    Map<String,Object> data = document.getData();
+                    for (String key : data.keySet()) {
+                        textViewEmployeeList.append("\n");
+                        textViewEmployeeList.append(key);
+                        textViewEmployeeList.append("\n");
+                        textViewEmployeeList.append((String) data.get(key));
+                        textViewEmployeeList.append("\n");
+                    }
+
+                } else {
+                }
+            }
+        });
 
         buttonControlPanel = (Button) findViewById(R.id.control_panel_button);
 
